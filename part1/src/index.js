c51b44f60d27775cdf03b2c6bdf71adebbf28a9a
Aemon <bot960528@gmail.com>
2021-01-13 10:08:07 +0800

    edit

--- part1/src/index.js
+++ part1/src/index.js
@@ -1,17 +1,42 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+const Header = (props) => <h1>{props.course}</h1>;
+
+const Part = (props) => (
+  <p>
+    {props.part} {props.exercises}
+  </p>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const Content = (props) => {
+  return (
+    <div>
+      {props.list.map((a, index) => (
+        <Part key={index} part={a.part} exercises={a.exercises}></Part>
+      ))}
+    </div>
+  );
+};
+
+const Total = (props) => (
+  <p>Number of exercises {props.exercisesList.reduce((a, b) => a + b)}</p>
+);
+
+const App = () => {
+  const course = "Half Stack application development";
+  const List = [
+    { part: "Fundamentals of React", exercises: 10 },
+    { part: "Using props to pass data", exercises: 7 },
+    { part: "State of a component", exercises: 14 },
+  ];
+  return (
+    <div>
+      <Header course={course} />
+      <Content list={List} />
+      <Total exercisesList={List.map((a) => a.exercises)}></Total>
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById("root"));
