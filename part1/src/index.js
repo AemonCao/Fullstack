234a459d6cd7b46f2f1957d03bdd5005ffbbad6c
Aemon <bot960528@gmail.com>
2021-01-13 17:43:13 +0800

    Conditional rendering

--- part1/src/index.js
+++ part1/src/index.js
@@ -1,25 +1,37 @@
 import React, { useState } from "react";
 import ReactDOM from "react-dom";
 
-const Display = ({ counter }) => <div>{counter}</div>;
+const History = ({ allClicks }) => {
+  if (allClicks.length === 0) {
+    return <div>the app is used by pressing the buttons</div>;
+  }
+  return <div> button press history: {allClicks.join(" ")}</div>;
+};
 
-const Button = ({ handleClick, text }) => (
-  <button onClick={handleClick}>{text}</button>
-);
+const Button = ({ onClick, text }) => <button onClick={onClick}>{text}</button>;
 
 const App = () => {
-  const [counter, setCounter] = useState(0);
+  const [left, setLeft] = useState(0);
+  const [right, setRight] = useState(0);
+  const [allClicks, setAll] = useState([]);
 
-  const increaseByOne = () => setCounter(counter + 1);
-  const decreaseByOne = () => setCounter(counter - 1);
-  const setToZero = () => setCounter(0);
+  const handleLeftClick = () => {
+    setAll(allClicks.concat("L"));
+    setLeft(left + 1);
+  };
+
+  const handleRightClick = () => {
+    setAll(allClicks.concat("R"));
+    setRight(right + 1);
+  };
 
   return (
     <div>
-      <Display counter={counter}></Display>
-      <Button handleClick={increaseByOne} text="plus"></Button>
-      <Button handleClick={setToZero} text="zero"></Button>
-      <Button handleClick={decreaseByOne} text="minus"></Button>
+      {left}
+      <Button onClick={handleLeftClick} text="left"></Button>
+      <Button onClick={handleRightClick} text="right"></Button>
+      {right}
+      <History allClicks={allClicks}></History>
     </div>
   );
 };
