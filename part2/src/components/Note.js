5300ca01c14d06d329a4de4612ebf1405180b4dd
Aemon <bot960528@gmail.com>
2021-01-15 16:52:11 +0800

    Filtering Displayed Elements

--- part2/src/components/Note.js
+++ part2/src/components/Note.js
@@ -1,5 +1,9 @@
 import React from "react";
 
-const Note = ({ note }) => <li>{note.content}</li>;
+const Note = ({ note }) => (
+  <li>
+    {note.important ? <b>{note.content}</b> : <span>{note.content}</span>}
+  </li>
+);
 
 export default Note;
