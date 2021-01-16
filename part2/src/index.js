b920630c089ef8f0a75e88241600476de502e776
Aemon <bot960528@gmail.com>
2021-01-16 16:01:51 +0800

    Axios and promises

--- part2/src/index.js
+++ part2/src/index.js
@@ -1,5 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import axios from "axios";
 import App from "./App";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+axios.get("http://localhost:3001/notes").then((response) => {
+  const notes = response.data;
+  ReactDOM.render(<App notes={notes} />, document.getElementById("root"));
+});
