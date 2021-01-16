e853fcb17e6ab952acadaaab1956328444362e87
Aemon <bot960528@gmail.com>
2021-01-16 18:08:29 +0800

    2.12

--- part2/src/App.js
+++ part2/src/App.js
@@ -1,59 +1,60 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import Filter from "./components/Filter";
-import PersonForm from "./components/PersonForm";
-import Persons from "./components/Persons";
+
+const Countries = ({ countries }) => {
+  if (countries.length > 10) {
+    return <div>Too mant matches, specify another filter</div>;
+  } else if (countries.length > 1 && countries.length <= 10) {
+    return countries.map((country) => (
+      <div key={country.name}>{country.name}</div>
+    ));
+  } else if (countries.length === 1) {
+    const country = countries[0];
+    return (
+      <div>
+        <h1>{country.name}</h1>
+        <div>capital {country.capital}</div>
+        <div>population {country.population}</div>
+        <h3>languages</h3>
+        <ul>
+          {country.languages.map((language) => (
+            <li key={language.name}>{language.name}</li>
+          ))}
+        </ul>
+        <img src={country.flag} alt={country.name} />
+      </div>
+    );
+  } else {
+    return <div>404</div>;
+  }
+};
 
 const App = () => {
-  const [persons, setPersons] = useState([]);
-  const [newName, setNewName] = useState("");
-  const [newNumber, setnewNumber] = useState("");
-  const [filterWord, setFilterWord] = useState("");
+  const [countries, setCountries] = useState([]);
+  const [filterCountryName, setFilterCountryName] = useState("");
 
   useEffect(() => {
-    axios.get("http://localhost:3001/persons").then((response) => {
-      setPersons(response.data);
+    axios.get("https://restcountries.eu/rest/v2/all").then((response) => {
+      setCountries(response.data);
     });
   }, []);
 
-  const filterPersons = persons.filter(
-    (person) =>
-      person.name.toUpperCase().indexOf(filterWord.toUpperCase()) !== -1
+  const onFilterCountryNameInputChange = (event) => {
+    setFilterCountryName(event.target.value);
+  };
+
+  const filterCountries = countries.filter(
+    (country) =>
+      country.name.toUpperCase().indexOf(filterCountryName.toUpperCase()) > -1
   );
-
-  const addPerson = (event) => {
-    event.preventDefault();
-    if (persons.map((person) => person.name).includes(newName)) {
-      alert(`${newName} is already added to phonebook`);
-    } else {
-      setPersons(persons.concat({ name: newName, number: newNumber }));
-    }
-  };
-
-  const handleFilterWordChange = (event) => {
-    setFilterWord(event.target.value);
-  };
-
-  const handleNewNameChange = (event) => {
-    setNewName(event.target.value);
-  };
-
-  const handleNewNumberChange = (event) => {
-    setnewNumber(event.target.value);
-  };
 
   return (
     <div>
-      <h2>Phonebook</h2>
-      <Filter onFilterInputChange={handleFilterWordChange.bind(this)} />
-      <h2>add a new</h2>
-      <PersonForm
-        onFormSubmit={addPerson.bind(this)}
-        onNameInputChange={handleNewNameChange.bind(this)}
-        onNumberInputChange={handleNewNumberChange.bind(this)}
-      />
-      <h2>Numbers</h2>
-      <Persons persons={filterPersons} />
+      <div>
+        find countries
+        <input onChange={onFilterCountryNameInputChange} />
+      </div>
+      <Countries countries={filterCountries} />
     </div>
   );
 };
