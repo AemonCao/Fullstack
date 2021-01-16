276582aa2fea03e2d296720bc8c7464f25916097
Aemon <bot960528@gmail.com>
2021-01-16 16:56:43 +0800

    2.11

--- part2/src/App.js
+++ part2/src/App.js
@@ -1,59 +1,57 @@
-import React, { useState, useEffect } from "react";
-import axios from "axios";
-import Note from "./components/Note";
+import React, { useState } from "react";
+import Filter from "./components/Filter";
+import PersonForm from "./components/PersonForm";
+import Persons from "./components/Persons";
 
 const App = () => {
-  const [notes, setNotes] = useState([]);
-  const [newNote, setNewNote] = useState("");
-  const [showAll, setShowAll] = useState(true);
+  const [persons, setPersons] = useState([
+    { name: "Arto Hellas", number: "040-123456" },
+    { name: "Ada Lovelace", number: "39-44-5323523" },
+    { name: "Dan Abramov", number: "12-43-234345" },
+    { name: "Mary Poppendieck", number: "39-23-6423122" },
+  ]);
+  const [newName, setNewName] = useState("");
+  const [newNumber, setnewNumber] = useState("");
+  const [filterWord, setFilterWord] = useState("");
 
-  const hook = () => {
-    console.log("effect");
-    axios.get("http://localhost:3001/notes").then((response) => {
-      console.log("promise fulfilled");
-      setNotes(response.data);
-    });
-  };
+  const filterPersons = persons.filter(
+    (person) =>
+      person.name.toUpperCase().indexOf(filterWord.toUpperCase()) !== -1
+  );
 
-  useEffect(hook, []);
-
-  console.log("render", notes.length, "notes");
-
-  const addNote = (event) => {
+  const addPerson = (event) => {
     event.preventDefault();
-    const noteObject = {
-      content: newNote,
-      date: new Date().toISOString(),
-      important: Math.random() < 0.5,
-      id: notes.length + 1,
-    };
-    setNotes(notes.concat(noteObject));
-    setNewNote("");
+    if (persons.map((person) => person.name).includes(newName)) {
+      alert(`${newName} is already added to phonebook`);
+    } else {
+      setPersons(persons.concat({ name: newName, number: newNumber }));
+    }
   };
 
-  const handleNoteChange = (event) => {
-    setNewNote(event.target.value);
+  const handleFilterWordChange = (event) => {
+    setFilterWord(event.target.value);
   };
 
-  const notesToShow = showAll ? notes : notes.filter((note) => note.important);
+  const handleNewNameChange = (event) => {
+    setNewName(event.target.value);
+  };
+
+  const handleNewNumberChange = (event) => {
+    setnewNumber(event.target.value);
+  };
 
   return (
     <div>
-      <h1>Notes</h1>
-      <div>
-        <button onClick={() => setShowAll(!showAll)}>
-          show {showAll ? "important" : "all"}
-        </button>
-      </div>
-      <ul>
-        {notesToShow.map((note) => (
-          <Note key={note.id} note={note} />
-        ))}
-      </ul>
-      <form onSubmit={addNote}>
-        <input value={newNote} onChange={handleNoteChange} />
-        <button type="submit">save</button>
-      </form>
+      <h2>Phonebook</h2>
+      <Filter onFilterInputChange={handleFilterWordChange.bind(this)} />
+      <h2>add a new</h2>
+      <PersonForm
+        onFormSubmit={addPerson.bind(this)}
+        onNameInputChange={handleNewNameChange.bind(this)}
+        onNumberInputChange={handleNewNumberChange.bind(this)}
+      />
+      <h2>Numbers</h2>
+      <Persons persons={filterPersons} />
     </div>
   );
 };
