549a893dbc687e8eec4d0ef8097bedadff797966
Aemon <bot960528@gmail.com>
2021-01-16 18:42:28 +0800

    2.13

--- part2/src/App.js
+++ part2/src/App.js
@@ -1,29 +1,49 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 
+const Country = ({ country }) => {
+  const [showInfo, setShowInfo] = useState(false);
+
+  const showButtonClickHandler = () => {
+    setShowInfo(!showInfo);
+  };
+
+  return (
+    <div>
+      <span>{country.name}</span>
+      <button onClick={showButtonClickHandler}>
+        {showInfo ? "hide" : "show"}
+      </button>
+      {showInfo ? (
+        <>
+          <h1>{country.name}</h1>
+          <div>capital {country.capital}</div>
+          <div>population {country.population}</div>
+          <h3>languages</h3>
+          <ul>
+            {country.languages.map((language) => (
+              <li key={language.name}>{language.name}</li>
+            ))}
+          </ul>
+          <img src={country.flag} alt={country.name} />
+        </>
+      ) : (
+        <></>
+      )}
+    </div>
+  );
+};
+
 const Countries = ({ countries }) => {
-  if (countries.length > 10) {
+  if (countries.length > 100) {
     return <div>Too mant matches, specify another filter</div>;
-  } else if (countries.length > 1 && countries.length <= 10) {
+  } else if (countries.length > 1 && countries.length <= 100) {
     return countries.map((country) => (
-      <div key={country.name}>{country.name}</div>
+      <Country key={country.name} country={country} />
     ));
   } else if (countries.length === 1) {
     const country = countries[0];
-    return (
-      <div>
-        <h1>{country.name}</h1>
-        <div>capital {country.capital}</div>
-        <div>population {country.population}</div>
-        <h3>languages</h3>
-        <ul>
-          {country.languages.map((language) => (
-            <li key={language.name}>{language.name}</li>
-          ))}
-        </ul>
-        <img src={country.flag} alt={country.name} />
-      </div>
-    );
+    return <Country country={country} />;
   } else {
     return <div>404</div>;
   }
