254262decd4e04a3f4f9978f1d7cabfcb5c0d728
Aemon <bot960528@gmail.com>
2021-01-18 21:40:58 +0800

    3.7&3.8

--- part3/person/index.js
+++ part3/person/index.js
@@ -1,7 +1,10 @@
-// const { request, response } = require("express");
 const express = require("express");
+const morgan = require("morgan");
 const app = express();
 
+morgan.token('json', function (req, res) { return JSON.stringify(req.body) })
+
+app.use(morgan(":method :url :status :res[content-length] - :response-time ms :json"));
 app.use(express.json());
 
 let persons = [
