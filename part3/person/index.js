5b8e1b9413f1bc7e05a82650c5e61f2238c5b5ab
Aemon <bot960528@gmail.com>
2021-01-18 21:01:47 +0800

    3.4

--- part3/person/index.js
+++ part3/person/index.js
@@ -47,6 +47,12 @@ app.get("/api/persons/:id", (request, response) => {
   }
 });
 
+app.delete("/api/persons/:id", (requset, response) => {
+  const id = Number(requset.params.id);
+  persons = persons.filter((person) => person.id !== id);
+  response.status(204).end();
+});
+
 const PORT = 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
