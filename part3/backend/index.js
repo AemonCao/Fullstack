c922d911c8fdb46d7a61b986688b8233dc59b6a4
Aemon <bot960528@gmail.com>
2021-01-18 19:41:44 +0800

    Receiving data

--- part3/backend/index.js
+++ part3/backend/index.js
@@ -2,6 +2,8 @@ const { request } = require("express");
 const express = require("express");
 const app = express();
 
+app.use(express.json());
+
 let notes = [
   {
     id: 1,
@@ -23,12 +25,17 @@ let notes = [
   },
 ];
 
-app.get("/", (req, res) => {
+const generateId = () => {
+  const maxId = notes.length > 0 ? Math.max(...notes.map((n) => n.id)) : 0;
+  return maxId + 1;
+};
+
+app.get("/", (request, response) => {
   res.send("<h1>Hello World!</h1>");
 });
 
-app.get("/api/notes", (req, res) => {
-  res.json(notes);
+app.get("/api/notes", (request, response) => {
+  response.json(notes);
 });
 
 app.get("/api/notes/:id", (request, response) => {
@@ -45,6 +52,27 @@ app.delete("/api/notes/:id", (request, response) => {
   const id = Number(request.params.id);
   notes = notes.filter((note) => note.id !== id);
   response.status(204).end();
+});
+
+app.post("/api/notes", (request, response) => {
+  const body = request.body;
+
+  if (!body.content) {
+    return response.status(400).json({
+      error: "content missing",
+    });
+  }
+
+  const note = {
+    content: body.content,
+    important: body.important || false,
+    date: new Date(),
+    id: generateId(),
+  };
+
+  notes = notes.concat(note);
+  
+  response.json(note);
 });
 
 const PORT = 3001;
