0828f8c595910baaddc7fa07180b7d09d2f4f57e
Aemon <bot960528@gmail.com>
2021-01-31 21:48:03 +0800

    Verifying frontend and backend integration

--- part2/src/index.js
+++ part2/src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./App";
+// import App from "./App";
+import NoteApp from "./NoteApp";
 import "./index.css";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(<NoteApp />, document.getElementById("root"));
--- part3/backend/index.js
+++ part3/backend/index.js
@@ -1,9 +1,12 @@
 require("dotenv").config();
 const express = require("express");
+const cors = require("cors");
 const morgan = require("morgan");
 const Note = require("./models/note");
 const app = express();
 
+// 跨域
+app.use(cors());
 app.use(morgan("tiny"));
 app.use(express.json());
 
