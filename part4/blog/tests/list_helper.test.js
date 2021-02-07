d70171d26f98efa818fda6b41fc5326e66f93ccb
Aemon <bot960528@gmail.com>
2021-02-08 03:05:52 +0800

    4.5

--- part4/blog/tests/list_helper.test.js
+++ part4/blog/tests/list_helper.test.js
@@ -12,12 +12,9 @@ describe('dummy', () => {
 describe('total likes', () => {
   const listWithOneBlog = [
     {
-      _id: '5a422aa71b54a676234d17f8',
-      title: 'Go To Statement Considered Harmful',
-      author: 'Edsger W. Dijkstra',
-      url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
       likes: 5,
-      __v: 0
+      title: 'Go To Statement Considered Harmful',
+      author: 'Edsger W. Dijkstra'
     }
   ]
 
@@ -34,33 +31,49 @@ describe('total likes', () => {
 
   const listWithThreeBlog = [
     {
-      likes: 500,
+      likes: 100,
       title: 'Hello World!',
       author: 'Aemon Cao',
-      url: 'aemon.top',
-      __v: 0,
-      id: '60202905fe70d82b70f95292'
     },
     {
-      likes: 500,
+      likes: 200,
       title: 'Hello World!',
       author: 'Aemon Cao',
-      url: 'aemon.top',
-      __v: 0,
-      id: '60202917fe70d82b70f95293'
     },
     {
-      likes: 500,
+      likes: 300,
       title: 'Hello World!',
       author: 'Aemon Cao',
-      url: 'aemon.top',
-      __v: 0,
-      id: '6020321c0f066e5a64705d42'
     }
   ]
 
-  test('when list has only three blog, equals the likes of that', () => {
+  test('when list has three blog, equals the likes of that', () => {
     const result = listHelper.totalLikes(listWithThreeBlog)
-    expect(result).toBe(1500)
+    expect(result).toBe(600)
+  })
+})
+
+describe('favorite blog', () => {
+  const listWithThreeBlog = [
+    {
+      likes: 100,
+      title: 'Hello World!',
+      author: 'Aemon Cao',
+    },
+    {
+      likes: 200,
+      title: 'Hello World!',
+      author: 'Aemon Cao',
+    },
+    {
+      likes: 300,
+      title: 'Hello World!',
+      author: 'Aemon Cao',
+    }
+  ]
+
+  test('when list has three blog, equals the favorite blog ', () => {
+    const result = listHelper.favoriteBlog(listWithThreeBlog)
+    expect(result).toEqual({ likes: 300, title: 'Hello World!', author: 'Aemon Cao', })
   })
 })
--- part4/blog/utils/list_helper.js
+++ part4/blog/utils/list_helper.js
@@ -1,3 +1,5 @@
+const _ = require('lodash')
+
 const dummy = blogs => {
   return 1
 }
@@ -6,6 +8,15 @@ const totalLikes = blogs => {
   return blogs.map(blog => blog.likes).reduce((a, b) => a + b, 0)
 }
 
+const favoriteBlog = blogs => {
+  // https://xiaoxiami.gitbook.io/lodash/math/maxby
+  return _.maxBy(blogs, 'likes')
+}
+
+const mostBlogs = blogs => {
+  // todo: https://fullstackopen.com/zh/part4/%E4%BB%8E%E5%90%8E%E7%AB%AF%E7%BB%93%E6%9E%84%E5%88%B0%E6%B5%8B%E8%AF%95%E5%85%A5%E9%97%A8#exercises-4-3-4-7
+}
+
 module.exports = {
-  dummy, totalLikes
+  dummy, totalLikes, favoriteBlog
 }
