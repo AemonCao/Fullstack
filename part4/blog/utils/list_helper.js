ff4860336645b4c58ea83b412f8adc7035dc6d5f
Aemon <bot960528@gmail.com>
2021-02-08 03:06:42 +0800

    4.4

--- part4/blog/utils/list_helper.js
+++ part4/blog/utils/list_helper.js
@@ -13,10 +13,6 @@ const favoriteBlog = blogs => {
   return _.maxBy(blogs, 'likes')
 }
 
-const mostBlogs = blogs => {
-  // todo: https://fullstackopen.com/zh/part4/%E4%BB%8E%E5%90%8E%E7%AB%AF%E7%BB%93%E6%9E%84%E5%88%B0%E6%B5%8B%E8%AF%95%E5%85%A5%E9%97%A8#exercises-4-3-4-7
-}
-
 module.exports = {
   dummy, totalLikes, favoriteBlog
 }
