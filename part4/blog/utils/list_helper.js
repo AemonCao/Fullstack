5344e622a028ca4ef9a6b50cc2618cf2fd240fbf
Aemon <bot960528@gmail.com>
2021-02-21 21:28:04 +0800

    4.6

--- part4/blog/utils/list_helper.js
+++ part4/blog/utils/list_helper.js
@@ -13,6 +13,14 @@ const favoriteBlog = blogs => {
   return _.maxBy(blogs, 'likes')
 }
 
+const mostBlogs = blogs => {
+  var temp = []
+  _.forIn(_.countBy(blogs, item => item.author), (value, key) => {
+    temp.push({ author: key, blogs: value })
+  })
+  return _.maxBy(temp, 'blogs')
+}
+
 module.exports = {
-  dummy, totalLikes, favoriteBlog
+  dummy, totalLikes, favoriteBlog, mostBlogs
 }
